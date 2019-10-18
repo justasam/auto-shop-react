0cf18ec8ea8b20b277c1fdeb1ae50e5bf3d3ac24
justasam <samuolis.just@gmail.com>
2019-10-18 20:22:30 +0100

    First take at blurred main image, currently black overlay instead of blur

---
+++ src/components/BlurryImage/BlurryImage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const BlurryImage = ({src, width='100vw'}) => {
+  return (
+    <div style={{
+      position: 'relative',
+      margin: 0,
+      padding: 0,
+    }}>
+      <img src={src} alt="blurred" style={{
+        position: 'relative',
+        width: width,
+      }}/>
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: width,
+        height: '100%',
+        backgroundColor: 'black',
+        opacity: 0.5
+      }}></div>
+    </div>
+  )
+}
+
+export default BlurryImage;
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { BlurryImage } from '../../components/BlurryImage';
 
 const HomeScreen = props => {
   return (
     <div>
+      <BlurryImage src="https://www.tesla.com/ns_videos/commerce/content/dam/tesla/tesla-shop-marketing-imagery/hero-carousel/wall-connector.jpg" />
       <h3>HomeScreen View</h3>
     </div>
   )
