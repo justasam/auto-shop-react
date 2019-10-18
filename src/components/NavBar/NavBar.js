5c92a835bb863e7c4a9a3dfb603e9db6a69c0e0c
justasam <samuolis.just@gmail.com>
2019-10-18 19:22:35 +0100

    Added styling for navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'react-feather';
+import './index.css';
 
 const NavBar = () => {
   return (
-    <div>
-      <h5>NavBar</h5>
-      <ul>
-        <li><Link to="/Home">Home</Link></li>
+    <div className="navbar">
+      <Link to="/Home" className="navbar_logo">
+        <span className="bigtext">Old Volks Home | </span>
+        <span className="mediumtext">SHOP</span>
+      </Link>
+      <ul className="navbar_links_main mediumtext transformcenter">
+        <li className="navbar_link"><Link to="/Home">VEHICLES</Link></li>
+        <li className="navbar_link"><Link to="/Home">PARTS</Link></li>
+        <li className="navbar_link"><Link to="/Home">SERVICES</Link></li>
+        <li className="navbar_link"><Link to="/Home">BRANCHES</Link></li>
+        <li className="navbar_link"><Link to="/Home">RESOURCES</Link></li>
+      </ul>
+      <ul className="navbar_links_sep mediumtext">
+        <li className="navbar_link" style={{float: 'left'}}><Link to="/Home">SIGN IN</Link></li>
+        <li className="navbar_link"><Link to="/Home"><ShoppingCart height={52} /></Link></li>
       </ul>
     </div>
   )
