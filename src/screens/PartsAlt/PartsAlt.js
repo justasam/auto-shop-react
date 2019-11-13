e37b55b8f203039670e9b08c73d868f9a4c19511
justas <samuolis.just@gmail.com>
2019-11-13 22:06:25 +0000

    Did some more work on parts page

--- src/screens/PartsAlt/PartsAlt.js
+++ src/screens/PartsAlt/PartsAlt.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { ProductCardAlt } from '../../components/ProductCardAlt';
+import { DropdownAlt } from '../../components/Inputs';
 import './index.css';
 
 const PartsAlt = props => {
   return (
     <div>
-      <h3>PartsAlt</h3>
+      <h3 style={{
+        padding: '10px 20px',
+        fontSize: 22
+      }}>PartsAlt</h3>
+      <div className='parts_alt_filters'>
+        <DropdownAlt title='MODEL:' name='model' options={[
+          {value: 'golf_3', name: 'Golf 3'},
+          {value: 'loremipsum', name: 'lorem'}
+        ]} />
+        <DropdownAlt title='PRICE MIN:' name='model' options={[
+          {value: '50', name: '50$'},
+          {value: 'loremipsum', name: 'lorem'}
+        ]} />
+        <DropdownAlt title='PRICE MAX:' name='model' options={[
+          {value: '200', name: '200$'},
+          {value: 'loremipsum', name: 'lorem'}
+        ]} />
+      </div>
       <div style={{
         display: 'grid',
         gridTemplateColumns: 'repeat(auto-fit, minmax(500px, 1fr))',
