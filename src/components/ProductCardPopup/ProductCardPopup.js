a3aa160a2fcae850ad260a465590a1ad531ea865
justas <samuolis.just@gmail.com>
2019-11-16 18:47:49 +0000

    Added styling and more logic for ProductCard Popup

--- src/components/ProductCardPopup/ProductCardPopup.js
+++ src/components/ProductCardPopup/ProductCardPopup.js
@@ -5,15 +5,49 @@ import { CarouselThumbnails } from '../CarouselThumbnails';
 
 
 
-const ProductCardPopup = props => {
+const ProductCardPopup = () => {
   return (
-    <div>
-      <h3>ProductCardPopup</h3>
-      <CarouselThumbnails style={{
-        maxWidth: 500,
-        height: 500,
-        margin: 20
-      }} />
+    <div style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'rgba(0,0,0,.5)'
+    }}>
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        gap: 20,
+        width: '1000px',
+        maxWidth: '80vw',
+        height: '1000px',
+        maxHeight: '80vw',
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 4
+      }} className='shadow'>
+        <div style={{
+          position: 'relative'
+        }}>
+          <CarouselThumbnails style={{
+            position: 'absolute',
+            display: 'block',
+            maxWidth: '100%',
+            overflow: 'hidden',
+            paddingBottom: 50
+          }} />
+        </div>
+        <div>
+          <h2>BMW X1 (2014)</h2>
+          <p>XDRIVE18D XLINE 2.0 5dr</p>
+          <h1>$11,800</h1>
+        </div>
+      </div>
     </div>
   )
 }
